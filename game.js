6959f848b0972699f9880fa9f3bc1b85c16cf177
germanespinosa <germanespinosa@gmail.com>
2020-02-27 11:58:56 -0600

    no message

--- game.js
+++ game.js
@@ -16,27 +16,6 @@ const down = {x:0,y:1};
 const moves = [left, right, down, up];
 var playGame = function(game){};
 
-var predator = {
-     x : 7,
-     y : 7,
-     randomMove: function (){
-          while (!predator.move(moves[Math.floor(Helpers.GetRandomInt(3))])) ;
-     },
-     move: function(move) {
-          if (!predator.checkMove(move)) return false;
-          predator.x += move.x;
-          predator.y += move.y;
-          return true;
-     },
-     checkMove: function(move){
-          return predator.x + move.x >= 0 &&
-              predator.y + move.y >= 0 &&
-              predator.x + move.x < Dungeon.map_size &&
-              predator.y + move.y < Dungeon.map_size &&
-              Dungeon.map[this.y + move.y][this.x + move.x] == 1;
-     }
-};
-
 playGame.prototype = {
      preload: function(){
           myimage = game.load.image("tile", "tile.png");
---
+++ predator.js
@@ -0,0 +1,20 @@
+var predator = {
+    x : 7,
+    y : 7,
+    randomMove: function (){
+        while (!predator.move(moves[Math.floor(Helpers.GetRandomInt(3))])) ;
+    },
+    move: function(move) {
+        if (!predator.checkMove(move)) return false;
+        predator.x += move.x;
+        predator.y += move.y;
+        return true;
+    },
+    checkMove: function(move){
+        return predator.x + move.x >= 0 &&
+            predator.y + move.y >= 0 &&
+            predator.x + move.x < Dungeon.map_size &&
+            predator.y + move.y < Dungeon.map_size &&
+            Dungeon.map[this.y + move.y][this.x + move.x] == 1;
+    }
+};
