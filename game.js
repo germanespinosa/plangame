b064026f75077c2c96da504c3e612bb00d3117db
germanespinosa <germanespinosa@gmail.com>
2020-02-29 09:38:04 -0600

    no message

--- game.js
+++ game.js
@@ -8,32 +8,6 @@ window.onload = function () {
      game.state.start("PlayGame");
 }
 
-var moves = {
-     stay : {x:0,y:0},
-     left : {x:-1,y:0},
-     right : {x:1,y:0},
-     up : {x:0,y:-1},
-     down : {x:0,y:1},
-     list: [],
-     isMove :function (a,b){
-          return a.x == b.x && a.y == b.y;
-     },
-     isRight(a){
-          return moves.isMove(a,moves.right);
-     },
-     isLeft(a){
-          return moves.isMove(a,moves.left);
-     },
-     isUp(a){
-          return moves.isMove(a,moves.up);
-     },
-     isDown(a){
-          return moves.isMove(a,moves.down);
-     }
-}
-
-moves.list = [moves.left, moves.right, moves.down, moves.up];
-
 var playGame = function(game){};
 
 let groups = {
