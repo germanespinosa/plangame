8663ad44486fc2b4d34cf8d97f38a4a97772350a
germanespinosa <germanespinosa@gmail.com>
2020-03-02 12:03:48 -0600

    no message

--- js/game_status.js
+++ js/game_status.js
@@ -2,31 +2,31 @@ let gameStatus = {
     titleTimeOut : 1000,
     refreshRate: 300,
     code: 0,
-    showMessage:function(message,size){
+    showMessage:function(message,size, tint = 0xFFFFFF){
         groups.status.removeAll();
-        groups.status.add(game.add.bitmapText(10, 100, '8bit','Drag me around !',34));
-        let sprite = game.add.sprite(game.width/2,game.height/2,message);
+        let sprite = game.add.bitmapText(game.width/2,game.height/2, '8bit',message,34);
         sprite.anchor.x = .5;
         sprite.anchor.y = .5;
-        let scale = game.width/game.cache.getImage(message).width*size;
+        sprite.tint = tint;
+        let scale = game.width*size/sprite.width;
         sprite.scale.setTo(scale,scale);
         groups.status.add(sprite);
         game.world.bringToTop(groups.status);
     },
     ready: function(){
         gameStatus.code = 1;
-        gameStatus.showMessage("ready",.5);
+        gameStatus.showMessage("ready",.5, 0xFF0000);
         setTimeout(gameStatus.set,gameStatus.titleTimeOut);
         maze.Generate();
     },
     set: function(){
         gameStatus.code = 2;
-        gameStatus.showMessage("set",.6);
+        gameStatus.showMessage("set",.6, 0xFFFF00);
         setTimeout(gameStatus.go,gameStatus.titleTimeOut);
     },
     go: function (){
         gameStatus.code = 3;
-        gameStatus.showMessage("go",.8);
+        gameStatus.showMessage("GO!",.80,0x00FF00);
         setTimeout(gameStatus.gameOn,gameStatus.titleTimeOut);
     },
     gameOn:function (){
