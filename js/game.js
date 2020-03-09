cce32694e46dd13d6b0cb26380cc5583f2aaa979
germanespinosa <germanespinosa@gmail.com>
2020-03-09 15:26:17 -0500

    no message

--- js/game.js
+++ js/game.js
@@ -1,5 +1,22 @@
 let game;
 
+function toggleFullScreen() {
+     var doc = window.document;
+     var docEl = doc.documentElement;
+
+     var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+     var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+     if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+          requestFullScreen.call(docEl);
+     }
+     else {
+          cancelFullScreen.call(doc);
+     }
+}
+
+toggleFullScreen();
+
 window.onload = function () {
      let body = document.body;
      let w = window.innerWidth;
