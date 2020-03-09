9d9991ed9f8207e9e0b7a6ef5209e8f28a994a8d
germanespinosa <germanespinosa@gmail.com>
2020-03-09 11:38:21 -0500

    no message

--- js/moves.js
+++ js/moves.js
@@ -1,25 +1,31 @@
 let moves = {
-    stay : {x:0,y:0},
-    left : {x:-1,y:0},
-    right : {x:1,y:0},
-    up : {x:0,y:-1},
-    down : {x:0,y:1},
+    stay: function () {return {x:0,y:0};},
+    left: function () {return {x:-1,y:0};},
+    right: function () {return {x:1,y:0};},
+    up: function () {return {x:0,y:-1};},
+    down: function () {return {x:0,y:1};},
     list: [],
     isMove :function (a,b){
         return a.x === b.x && a.y === b.y;
     },
     isRight(a){
-        return moves.isMove(a,moves.right);
+        return moves.isMove(a,moves.right());
     },
     isLeft(a){
-        return moves.isMove(a,moves.left);
+        return moves.isMove(a,moves.left());
     },
     isUp(a){
-        return moves.isMove(a,moves.up);
+        return moves.isMove(a,moves.up());
     },
     isDown(a){
-        return moves.isMove(a,moves.down);
+        return moves.isMove(a,moves.down());
+    },
+    isStay(a){
+        return moves.isMove(a,moves.stay());
+    },
+    copy(a){
+        return {x:a.x,y:a.y};
     }
 }
 
-moves.list = [moves.left, moves.right, moves.down, moves.up];
+moves.list = [moves.left(), moves.right(), moves.down(), moves.up()];
--- js/prey.js
+++ js/prey.js
@@ -1,51 +1,56 @@
 let prey = {
+    downKeys:[moves.stay()],
     x:0,
     y:0,
-    nextMove: {x:0,y:0},
+    lastMove: moves.stay(),
+    nextMove: function (){
+        return prey.downKeys[prey.downKeys.length-1];
+    },
     spriteName: null,
-    nextMoveSet: false,
-    leftIsDown: false,
-    rightIsDown: false,
-    upIsDown: false,
-    downIsDown: false,
+    addDownKey: function (key){
+        prey.downKeys.push(key);
+        prey.lastMove = moves.copy(key);
+    },
+    removeDownKey: function (key){
+        for( let i = 0; i < prey.downKeys.length; i++){
+            if ( prey.downKeys[i].x === key.x && prey.downKeys[i].y === key.y) {
+                prey.downKeys.splice(i, 1);
+            }
+        }
+    },
     onDownKeyDown : function (){
-        prey.downIsDown = true;
-        prey.setNextMove(moves.down);
+        prey.addDownKey(moves.down());
     },
     onUpKeyDown : function (){
-        prey.upIsDown = true;
-        prey.setNextMove(moves.up);
+        prey.addDownKey(moves.up());
     },
     onLeftKeyDown : function (){
-        prey.leftIsDown = true;
-        prey.setNextMove(moves.left);
+        prey.addDownKey(moves.left());
     },
     onRightKeyDown : function (){
-        prey.rightIsDown = true;
-        prey.setNextMove(moves.right);
+        prey.addDownKey(moves.right());
     },
     onDownKeyUp : function (){
-        prey.downIsDown = false;
+        prey.removeDownKey(moves.down());
     },
     onUpKeyUp : function (){
-        prey.upIsDown = false;
+        prey.removeDownKey(moves.up());
     },
     onLeftKeyUp : function (){
-        prey.leftIsDown = false;
+        prey.removeDownKey(moves.left());
     },
     onRightKeyUp : function (){
-        prey.rightIsDown = false;
+        prey.removeDownKey(moves.right());
     },
     start: function(){
         prey.scaleX = maze.tileSizeX/game.cache.getImage("prey").width;
         prey.scaleY = maze.tileSizeY/game.cache.getImage("prey").height;
-        prey.setNextMove(moves.stay);
         const pos = maze.copy(maze.world.startPosition);
         prey.x = pos.x;
         prey.y = pos.y;
     },
     addMove: function(pos){
-        var new_pos = {
+        let new_pos = {
             x:pos.x,
             y:pos.y
         };
@@ -53,30 +58,15 @@ let prey = {
         new_pos.y += prey.y;
         return new_pos;
     },
-    setNextMove: function(move){
-        prey.nextMoveSet = true;
-        prey.nextMove.x = move.x;
-        prey.nextMove.y = move.y;
-    },
     move: function() {
-        if (!prey.nextMoveSet){
-            if (moves.isLeft(prey.nextMove) && !prey.leftIsDown) {
-                prey.setNextMove(moves.stay);
-            }
-            if (moves.isRight(prey.nextMove) && !prey.rightIsDown) {
-                prey.setNextMove(moves.stay);
-            }
-            if (moves.isUp(prey.nextMove) && !prey.upIsDown) {
-                prey.setNextMove(moves.stay);
-            }
-            if (moves.isDown(prey.nextMove) && !prey.downIsDown) {
-                prey.setNextMove(moves.stay);
-            }
+        let nextMove = prey.nextMove();
+        if (moves.isStay(nextMove)) {
+            nextMove = prey.lastMove;
         }
-        prey.nextMoveSet = false;
-        if (!prey.checkMove(prey.nextMove)) return false;
-        prey.x += prey.nextMove.x;
-        prey.y += prey.nextMove.y;
+        prey.lastMove = moves.stay();
+        if (!prey.checkMove(nextMove)) return false;
+        prey.x += nextMove.x;
+        prey.y += nextMove.y;
         if (maze.equal(maze.world.goalPosition,prey)) {
             gameStatus.youWin();
             return false;
