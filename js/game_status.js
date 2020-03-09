10760a75f7dd1d40ffc6c9b02a494444b8e3f2b4
germanespinosa <germanespinosa@gmail.com>
2020-03-09 11:09:09 -0500

    no message

--- js/game_status.js
+++ js/game_status.js
@@ -1,11 +1,13 @@
 let gameStatus = {
+    speeds: [3,4,6], //moves per second
+    randomness: [2,4,8], // 50% random, 25% random, 12.5% random
     anchor:{x:0,y:0},
     updatePreyInterval: null,
     updatePredatorInterval: null,
     titleTimeOut : 1000,
     preyUpdateRate: 3,
     predatorUpdateRate: 4.5,
-    spinnersValues: [0,1,0,0],
+    spinnersValues: [0,1],
     code: 0,
     maps:[],
     predatorRandomness: 4, //%25 percent random
@@ -30,26 +32,18 @@ let gameStatus = {
         groups.status.add(back);
 
         gameStatus.code = 0;
-        gameStatus.showMessage("SURVIVAL",{w:.95,h:.1},0xFFFFFF,{x:.5,y:.10});
-        gameStatus.showMessage("MAP: ",{w:.3,h:.05},0xFFFFFF,{x:.025,y:.275}, {x:0,y:.5});
-        gameStatus.showMessage("MODE: ",{w:.3,h:.05},0xFFFFFF,{x:.025,y:.425}, {x:0,y:.5});
-        gameStatus.showMessage("SPEED: ",{w:.3,h:.05},0xFFFFFF,{x:.025,y:.575}, {x:0,y:.5});
-        gameStatus.showMessage("PURSUIT: ",{w:.3,h:.05},0xFFFFFF,{x:.025,y:.725}, {x:0,y:.5});
+        gameStatus.showMessage("SURVIVAL",{w:.95,h:.1},0xFFFFFF,{x:.51,y:.10});
+        gameStatus.showMessage("HABITAT: ",{w:.3,h:.05},0xFFFFFF,{x:.025,y:.25}, {x:0,y:.5});
+        gameStatus.showMessage("DIFFICULTY: ",{w:.4,h:.05},0xFFFFFF,{x:.025,y:.5}, {x:0,y:.5});
         let options = [];
         for (let i = 0;i < gameStatus.maps.length ;i++) options.push(gameStatus.maps[i].name);
-        gameStatus.mapSpinner = new Spinner(game.width * .325,game.height * .225,game.width *.675,game.height *.1,options,"8bit", groups.status);
+        gameStatus.mapSpinner = new Spinner(game.width * .1,game.height * .3,game.width *.8,game.height *.1,options,"8bit", groups.status);
         gameStatus.mapSpinner.selected = gameStatus.spinnersValues[0];
         gameStatus.mapSpinner.update();
-        gameStatus.modSpinner = new Spinner(game.width * .325,game.height * .375,game.width *.675,game.height *.1,["beginner","natural"],"8bit", groups.status);
+        gameStatus.modSpinner = new Spinner(game.width * .1,game.height * .55,game.width *.8,game.height *.1,["easy","natural", "hard"],"8bit", groups.status);
         gameStatus.modSpinner.selected = gameStatus.spinnersValues[1];
         gameStatus.modSpinner.update();
-        gameStatus.speedSpinner = new Spinner(game.width * .325,game.height * .525,game.width *.675,game.height *.1,["natural","fast", "fastest", "slowest", "slow"],"8bit", groups.status);
-        gameStatus.speedSpinner.selected = gameStatus.spinnersValues[2];
-        gameStatus.speedSpinner.update();
-        gameStatus.randomnessSpinner = new Spinner(game.width * .325,game.height * .675,game.width *.675,game.height *.1,["natural","aggressive", "fumbling"],"8bit", groups.status);
-        gameStatus.randomnessSpinner.selected = gameStatus.spinnersValues[3];
-        gameStatus.randomnessSpinner.update();
-        let playButton = gameStatus.showMessage("PLAY",{w:.8,h:.1},0xFFFFFF,{x:.5,y:.9});
+        let playButton = gameStatus.showMessage("PLAY",{w:.8,h:.1},0xFFFFFF,{x:.5,y:.85});
         playButton.anchor.x = .5;
         playButton.anchor.y = .5;
         groups.status.add(playButton);
@@ -59,14 +53,10 @@ let gameStatus = {
     ready: function(){
         gameStatus.spinnersValues[0] = gameStatus.mapSpinner.selected;
         gameStatus.spinnersValues[1] = gameStatus.modSpinner.selected;
-        gameStatus.spinnersValues[2] = gameStatus.speedSpinner.selected;
-        gameStatus.spinnersValues[3] = gameStatus.randomnessSpinner.selected;
 
-        let speeds = [3,4,6,1.5,2];
-        gameStatus.preyUpdateRate = speeds[gameStatus.speedSpinner.selected];
+        gameStatus.preyUpdateRate = gameStatus.speeds[gameStatus.modSpinner.selected];
         gameStatus.predatorUpdateRate = gameStatus.preyUpdateRate * 1.5;
-        let randomness = [4,8,2];
-        gameStatus.predatorRandomness = randomness[gameStatus.randomnessSpinner.selected];
+        gameStatus.predatorRandomness = gameStatus.randomness[gameStatus.modSpinner.selected];
         groups.status.removeAll(true);
         gameStatus.code = 1;
         gameStatus.showMessage("ready",{w:.5,h:1}, 0xFF0000);
@@ -75,8 +65,6 @@ let gameStatus = {
         maze.loadWorld(gameStatus.maps[gameStatus.mapSpinner.selected].file,0);
         gameStatus.mapSpinner.destroy();
         gameStatus.modSpinner.destroy();
-        gameStatus.speedSpinner.destroy();
-        gameStatus.randomnessSpinner.destroy();
     },
     set: function(){
         gameStatus.code = 2;
