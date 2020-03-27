4bc673ef68074666a0055f653521c502cd06a28c
alextlai <62082449+alextlai@users.noreply.github.com>
2020-03-27 15:05:40 -0500

    Update Menu Sizes/Positions

--- js/game_status.js
+++ js/game_status.js
@@ -45,13 +45,13 @@ let gameStatus = {
         gameStatus.showMessage("Played " + p + " times",{w:.75,h:.015},0xFFFFFF,{x:.45,y:.975}, {x:0,y:.5});
         let options = [];
         for (let i = 0;i < gameStatus.maps.length ;i++) options.push(gameStatus.maps[i].name);
-        gameStatus.mapSpinner = new Spinner(game.width * .215,game.height * .5,game.width *.55,game.height *.075,options,"8bit", groups.status);
+        gameStatus.mapSpinner = new Spinner(game.width * .215,game.height * .52,game.width *.55,game.height *.075,options,"8bit", groups.status);
         gameStatus.mapSpinner.selected = gameStatus.spinnersValues[0];
         gameStatus.mapSpinner.update();
         gameStatus.modSpinner = new Spinner(game.width * .215,game.height * .65,game.width *.55,game.height *.075,["easy","natural", "hard"],"8bit", groups.status);
         gameStatus.modSpinner.selected = gameStatus.spinnersValues[1];
         gameStatus.modSpinner.update();
-        let playButton = gameStatus.showMessage("PLAY",{w:.8,h:.1},0xD34F1D,{x:.5,y:.85});
+        let playButton = gameStatus.showMessage("PLAY",{w:.8,h:.1},0xD34F1D,{x:.4,y:.85});
         playButton.anchor.x = .5;
         playButton.anchor.y = .5;
         groups.status.add(playButton);
